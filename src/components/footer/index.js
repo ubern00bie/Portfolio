f3138b0a26a50f8c8075b19936d3cd8a4bf7a43b
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-11-01 22:37:25 -0600

    updated projects page

---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+const Footer = ()=> {
+  return(
+    <footer className = "footer">
+        <div className="container text-center">
+            <small>Jordan Benningfield 2020 &copy;</small>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = (children) => {
+  return(
+    
+    <nav className="row">
+      <nav className="col">
+        <header>   
+          <h1 id="navbar">Software Developer</h1>
+         
+        </header>
+      </nav>
+    </nav>
+  );
+}
+
+export default Header;
